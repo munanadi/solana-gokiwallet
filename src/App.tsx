bd3496513962674782f29b4e60f41bf8a312c25d
Aadhinana <ajibaa@gmail.com>
2021-09-21 01:34:16 +0530

    Added gokiwallet - works

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { WalletKitProvider } from "@gokiprotocol/walletkit";
+import { SnackbarProvider } from "notistack";
+import { Body } from "./Body";
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <WalletKitProvider
+      app={{
+        name: "Swap UI",
+      }}
+    >
+      <SnackbarProvider maxSnack={5} autoHideDuration={3000}>
+        <Body />
+      </SnackbarProvider>
+    </WalletKitProvider>
   );
-}
+};
 
 export default App;
